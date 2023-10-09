06c58bea48b69bd5cdbc3cb01aa34e49e474d3f8
Mr Giddy <143078458+Mrgiddy001@users.noreply.github.com>
2023-10-09 14:36:04 +0300

    rightangledtriangle.cpp

---
+++ dev.cpp
@@ -0,0 +1,9 @@
+double a,b,c;
+    cout<<"Enter Side A: ";
+    cin>>a;
+    cout<<"\nEnter side B: ";
+    cin>>b;
+    a=pow(a,2);
+    b=pow(b,2);
+    c=sqrt(a+b);
+    cout<<"\nSide C is: "<<c;
